1ee9029ea93efa8d10da07e8926e9126dbc585e0
Ciesielski Grzegorz <Grzegorz.Ciesielski@asseco.pl>
2020-02-25 13:34:32 +0100

    fix: reveal datasets submodule

--- spdg/__init__.py
+++ spdg/__init__.py
@@ -1,7 +1,8 @@
+import spdg.datasets as datasets
 import spdg.net as net
 import spdg.ngram as ngram
 
 from .net import Net
 from .ngram import Ngram
 
-__all__ = ['Ngram', 'ngram', 'Net', 'net']
+__all__ = ['Ngram', 'ngram', 'Net', 'net', 'datasets']
